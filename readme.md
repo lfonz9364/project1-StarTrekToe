c910f9043f1ee678379cae4df74af5079b739c4d
Alfons Caroles <cllin7787@gmail.com>
2017-02-24 00:44:16 +1100

    create readme.md and modified script

---
+++ readme.md
@@ -0,0 +1,23 @@
+# Project 1: Star Trek Toe
+
+### Technology Used
+* Jquery
+* CSS
+* HTML
+
+### Approach
+
+I choose to breakdown the projects into small problems and try to solve the small problems one by one. First of all, I solved the problem to ensure there will be an image show up when the cell clicked and prevent players to click at the same cell. Second, the game logic, based on the winning condition of Tic Tac Toe where each player must try to mark three cells adjacent to each other, either diagonally, vertically or horizontally, where translated into a combination of functions to continuously check whether the condition is met. Third, if the winning condition met then the screen will be covered with announcement of the winner's name. Last but not least, players could return to play the next round by closing the screen.
+
+### What I Learned
+
+Jquery is really helpful in optimizing code lines. There are features of Jquery that only required several words that could cover lines of Javascript functions. However, it is still require the programmer's creativity in using each one of the code to replace Javascript codes properly.
+
+### Unsolved Problems
+
+
+### To-Do-List
+
+### Future Features
+
+[Play Game](https://lfonz9364.github.io/projects/)
--- script/game.js
+++ script/game.js
@@ -1,134 +1,129 @@
 console.log('game');
 
-
+var playerOneName = $('input.playerOne').val();
+var playerTwoName = $('input.playerTwo').val();
+var playerOneSide = $('input.playerTwo').val();
+var playerTwoSide = $('input.playerTwo').val();
 var playerOneWinCounter = 0;
 var playerTwoWinCounter = 0;
 var roundNumber = 0;
-var playerOneScore = [];
-var playerTwoScore = [];
-
-
 
   var clearNames = function() {
     $('input.playerOne').val('');
     $('input.playerTwo').val('');
-  }
+  };
 
   var resetScore = function() {
     playerOneScore = [];
     playerTwoScore = [];
-  }
-
+  };
 
   var playerSelection = function(name1,name2){
     if ($('h2').attr('class') === 'playerOne') {
       $('h2').removeClass();
       $('h2').addClass('playerTwo');
-      $('h2').text(name2 + ' turn');
+      return $('h2').text(name2 + ' turn');
     } else if ($('h2').attr('class') === 'playerTwo'){
       $('h2').removeClass();
       $('h2').addClass('playerOne');
-      $('h2').text(name1 + ' turn');
+      return $('h2').text(name1 + ' turn');
     } else {
       $('h2').addClass('playerOne');
-      $('h2').text(name1 + ' turn');
+      return $('h2').text(name1 + ' turn');
     }
-  }
+  };
 
   var playerWhoWin = function(score) {
-    var winCondition = [['00','01','02'],['10','11','12'],['20','21','22'],
-                        ['20','11','22'],['00','11','22'],['00','10','20'],
-                        ['01','11','21'],['02','12','22']];
-    var x = false;
-    for (var i = 0; i < winCondition.length; i++){
-      if (score.join('') === winCondition[i].join('')) {
-        return x = true;
-      }
+    var x = 0;
+    if (score.includes('00') && score.includes('01') && score.includes('02')){
+       x = 1;
+    } else if (score.includes('10') && score.includes('11') && score.includes('12')) {
+      x = 1;
+    } else if (score.includes('20') && score.includes('21') && score.includes('22')){
+      x = 1;
+    } else if (score.includes('20') && score.includes('11') && score.includes('02')) {
+      x = 1;
+    } else if (score.includes('00') && score.includes('11') && score.includes('22')) {
+      x = 1;
+    } else if (score.includes('00') && score.includes('10') && score.includes('20')) {
+      x = 1;
+    } else if (score.includes('01') && score.includes('11') && score.includes('21')) {
+      x = 1;
+    } else if (score.includes('02') && score.includes('12') && score.includes('22')) {
+      x = 1;
     }
     return x;
-  }
+  };
 
-  var winOrLose = function(score1,score2,player1,player2) {
-    debugger;
-    if (playerWhoWin(score1)) {
-      $('.winner').text(player1 + " is the winner");
-      playerOneWinCounter += 1;
-      return true;
-    } else if (playerWhoWin(score2)) {
-      $('.winner').text(player2 + " is the winner");
-      playerTwoWinCounter += 1;
-      return true;
-    } else {
-      if(score1.length > 2){
-        playerOneScore.pop();
-      } else if (score2.length > 2) {
-        playerTwoScore.pop();
-      }
-      return false;
+  var clickEvent = function(event) {
+    var playerOneScore = [];
+    var playerTwoScore = [];
+    var $target = $(event.target);
+    var targetParent = $target.parent();
+    var targetPop = targetParent.parent();
+    var divIndex = targetParent.index();
+    var divParentIndex = targetPop.index();
+    var combIndex = String(divParentIndex) + String(divIndex);
+
+    if ($('h2').attr('class') == 'playerOne') {
+      playerOneScore.push(combIndex);
+      $target.attr('src', 'images/klingon_cruiser.jpg');
+    } else if ($('h2').attr('class') == 'playerTwo'){
+      playerTwoScore.push(combIndex);
+      $target.attr('src', 'images/borg_cube.jpg');
     }
-  }
+    playerSelection(playerOneName,playerTwoName);
+    if (playerOneScore.length > 2 || playerTwoScore.length > 2){
+      if (playerWhoWin(playerOneScore) == 1 ) {
+        $('.winner').text(playerOneName + " is the winner");
+        playerOneWinCounter += 1;
+        return openLightbox();
+      } else if (playerWhoWin(playerTwoScore) == 1) {
+        $('.winner').text(playerTwoName + " is the winner");
+        playerTwoWinCounter += 1;
+        return openLightbox();
+      }
+    }
+    return $target.off('click');
+  };
 
   var startGame = function() {
-    var playerOneName = $('input.playerOne').val();
-    var playerTwoName = $('input.playerTwo').val();
-    var playerOneSide = $('input.playerTwo').val();
-    var playerTwoSide = $('input.playerTwo').val();
     $('.playerOneName').text(playerOneName);
     $('.playerTwoName').text(playerTwoName);
     playerSelection(playerOneName,playerTwoName);
 
-    $('div.square img').click(function(event) {
-      var $target = $(event.target);
-      var targetParent = $target.parent();
-      var targetPop = targetParent.parent();
-      var divIndex = String(targetParent.index());
-      var divParentIndex = String(targetPop.index());
-      var combIndex = divParentIndex + divIndex;
-      
-      if ($('h2').attr('class') == 'playerOne') {
-        playerOneScore.push(combIndex);
-        $target.attr('src', 'images/klingon_cruiser.jpg');
-        playerOneScore.sort('');
-      } else if ($('h2').attr('class') == 'playerTwo'){
-        playerTwoScore.push(combIndex);
-        $target.attr('src', 'images/borg_cube.jpg');
-        playerTwoScore.sort('');
-      }
-      playerSelection(playerOneName,playerTwoName);
-      if (playerOneScore.length > 2 || playerTwoScore.length > 2){
-        if(winOrLose(playerOneScore,playerTwoScore,playerOneName,playerTwoName)) {
-          resetScore();
-          openLightbox();
-        }
-      }
-      $target.off('click');
-    });
-  }
+    $('div.square img').click(clickEvent(event));
+  };
 
 var openLightbox = function() {
   $('.lightbox').css('display', 'block');
   $('.winCountOne').text(playerOneWinCounter);
   $('.winCountTwo').text(playerTwoWinCounter);
-}
+};
 
 var closeLightbox = function() {
   $('.lightbox').css('display', 'none');
   resetGame();
-}
+};
 
 var openStartPage = function() {
   $('.startPage').css('display', 'block');
-}
+};
 
 var closeStartPage = function() {
   $('.startPage').css('display', 'none');
-}
+};
 
 var resetGame = function() {
+  debugger;
+  sessionStorage.clear();
+  localStorage.clear();
   roundNumber += 1;
+
   $('h2').removeClass();
   $('div.square img').attr('src', ' ');
-  $('div.square img').on('click',startGame());
-}
+  $('div.square img').on('click',clickEvent(event));
+  startGame();
+};
 
 $(document).one('ready',openStartPage());
