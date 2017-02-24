186faee1f13bb0f7f909724e39461525faee1268
Alfons Caroles <cllin7787@gmail.com>
2017-02-24 11:52:57 +1100

    Game function complete need more CSS

--- readme.md
+++ readme.md
@@ -7,7 +7,7 @@
 
 ### Approach
 
-I choose to breakdown the projects into small problems and try to solve the small problems one by one. First of all, I solved the problem to ensure there will be an image show up when the cell clicked and prevent players to click at the same cell. Second, the game logic, based on the winning condition of Tic Tac Toe where each player must try to mark three cells adjacent to each other, either diagonally, vertically or horizontally, where translated into a combination of functions to continuously check whether the condition is met. Third, if the winning condition met then the screen will be covered with announcement of the winner's name. Last but not least, players could return to play the next round by closing the screen.
+I choose to breakdown the projects into small problems and try to solve the small problems one by one. First of all, I solved the problem to ensure there will be a background image show up when the cell clicked and prevent players to click at the same square. Second, the game logic, based on the winning condition of Tic Tac Toe where each player must try to mark three cells adjacent to each other, either diagonally, vertically or horizontally, where translated into a combination of functions to continuously check whether the condition is met after each player click three square. Third, if the winning condition met then the screen will be covered with announcement of the winner's name. Last but not least, players could return to play the next round by closing the screen.
 
 ### What I Learned
 
@@ -15,9 +15,16 @@ Jquery is really helpful in optimizing code lines. There are features of Jquery 
 
 ### Unsolved Problems
 
+The current version have one not yet solved problem which is the way to check winning condition when the size of the board changes.
 
 ### To-Do-List
 
+* Add Timer
+* Custom board sizing
+
 ### Future Features
+
+* Timer for every round
+* Capability to customise box size
 
 [Play Game](https://lfonz9364.github.io/projects/)
--- script/game.js
+++ script/game.js
@@ -1,9 +1,7 @@
 console.log('game');
 
-var playerOneName = $('input.playerOne').val();
-var playerTwoName = $('input.playerTwo').val();
-var playerOneSide = $('input.playerTwo').val();
-var playerTwoSide = $('input.playerTwo').val();
+var playerOneScore = [];
+var playerTwoScore = [];
 var playerOneWinCounter = 0;
 var playerTwoWinCounter = 0;
 var roundNumber = 0;
@@ -55,45 +53,60 @@ var roundNumber = 0;
     return x;
   };
 
-  var clickEvent = function(event) {
-    var playerOneScore = [];
-    var playerTwoScore = [];
-    var $target = $(event.target);
-    var targetParent = $target.parent();
-    var targetPop = targetParent.parent();
-    var divIndex = targetParent.index();
-    var divParentIndex = targetPop.index();
-    var combIndex = String(divParentIndex) + String(divIndex);
-
-    if ($('h2').attr('class') == 'playerOne') {
-      playerOneScore.push(combIndex);
-      $target.attr('src', 'images/klingon_cruiser.jpg');
-    } else if ($('h2').attr('class') == 'playerTwo'){
-      playerTwoScore.push(combIndex);
-      $target.attr('src', 'images/borg_cube.jpg');
-    }
-    playerSelection(playerOneName,playerTwoName);
-    if (playerOneScore.length > 2 || playerTwoScore.length > 2){
-      if (playerWhoWin(playerOneScore) == 1 ) {
-        $('.winner').text(playerOneName + " is the winner");
-        playerOneWinCounter += 1;
-        return openLightbox();
-      } else if (playerWhoWin(playerTwoScore) == 1) {
-        $('.winner').text(playerTwoName + " is the winner");
-        playerTwoWinCounter += 1;
-        return openLightbox();
-      }
-    }
-    return $target.off('click');
-  };
+var selectCharacter = function(value) {
+  if (value === '1') {
+    return 'url(images/klingon_cruiser.jpg)';
+  } else if (value === '2') {
+    return 'url(images/borg_cube.jpg)';
+  } else if (value === '3') {
+    return 'url(images/romulan.jpg)';
+  } else if (value === '4') {
+    return 'url(images/vulcan.jpg)';
+  }
+}
 
   var startGame = function() {
+    var playerOneName = $('.playerOne').val();
+    var playerTwoName = $('.playerTwo').val();
+    var playerOneSide = $('.categoryOne').val();
+    var playerTwoSide = $('.categoryTwo').val();
+    var playerOneAvatar = selectCharacter(playerOneSide);
+    var playerTwoAvatar = selectCharacter(playerTwoSide);
     $('.playerOneName').text(playerOneName);
     $('.playerTwoName').text(playerTwoName);
     playerSelection(playerOneName,playerTwoName);
 
-    $('div.square img').click(clickEvent(event));
-  };
+    $('.square').click(function(event) {
+      var $target = $(event.target);
+      var targetParent = $target.parent();
+      var divIndex = $target.index();
+      var divParentIndex = targetParent.index();
+      var combIndex = String(divParentIndex) + String(divIndex);
+      debugger;
+      if ($('h2').attr('class') == 'playerOne') {
+        playerOneScore.push(combIndex);
+        $target.css({backgroundImage: playerOneAvatar});
+      } else if ($('h2').attr('class') == 'playerTwo'){
+        playerTwoScore.push(combIndex);
+        $target.css({backgroundImage: playerTwoAvatar});
+      }
+      playerSelection(playerOneName,playerTwoName);
+      if (playerOneScore.length > 2 || playerTwoScore.length > 2){
+        if (playerWhoWin(playerOneScore) == 1 ) {
+          $('.winner').text(playerOneName + " is the winner");
+          playerOneWinCounter += 1;
+          resetScore();
+          return openLightbox();
+        } else if (playerWhoWin(playerTwoScore) == 1) {
+          $('.winner').text(playerTwoName + " is the winner");
+          playerTwoWinCounter += 1;
+          resetScore();
+          return openLightbox();
+        }
+      }
+      return $target.attr('disabled','disabled')
+  });
+}
 
 var openLightbox = function() {
   $('.lightbox').css('display', 'block');
@@ -115,15 +128,9 @@ var closeStartPage = function() {
 };
 
 var resetGame = function() {
-  debugger;
-  sessionStorage.clear();
-  localStorage.clear();
   roundNumber += 1;
-
-  $('h2').removeClass();
-  $('div.square img').attr('src', ' ');
-  $('div.square img').on('click',clickEvent(event));
-  startGame();
+  $('.square').css({backgroundImage: ''});
+  $('.square').removeAttr('disabled');
 };
 
 $(document).one('ready',openStartPage());
